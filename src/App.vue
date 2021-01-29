70c25101fea06e7fbc063d26595daf13968a70b4
is_ryo_m1 <is_ryo@is-ryo-m1noMacBook-Pro.local>
2021-01-29 10:22:26 +0900

    update packages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">
+  <div class="h-screen w-full">
     <router-view />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="home">
+  <div class="home flex flex-col justify-center items-center">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
