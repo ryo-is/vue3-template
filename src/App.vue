7882990ff86842dd6856caad195636f3117ceb8d
is_ryo_m1 <is_ryo@is-ryo-m1noMacBook-Pro.local>
2021-01-29 10:05:37 +0900

    remove source code

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/todo">Todo</router-link>
-    </div>
     <router-view />
   </div>
 </template>
